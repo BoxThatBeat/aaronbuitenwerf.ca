a0e472f54129243b8fb51def75e8dd1a7ee858bd
BoxThatBeat <buitenwerfa@hotmail.ca>
2019-12-23 14:37:18 -0500

    Added a placeholder image and text over top. The image centers itself depending on the size of the browser widow.

---
+++ public/js/client.js
@@ -0,0 +1,1 @@
+ScrollReveal().reveal('.lg-heading-top', { distance: '50px' });
--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const fs = require('fs');
 const https = require('https');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Working on it.')
-})
+app.set("view engine", "pug");
+app.use(express.static("public"));
+
+app.get('/', function(req, res, next) {
+	res.status("200").render("index");
+	return;
+});
 
 
 
-https.createServer({ //read certificates and private key in
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-}, app).listen(3001, function () {
-  console.log('Server listening on https://aaronbuitenwerf.ca/')
+https.createServer({ //read certificates and private key to be used for https
+  key: fs.readFileSync('key.key'),
+  cert: fs.readFileSync('cert.cert')
+}, app).listen(3000, function () {
+  console.log('Server listening on https://localhost:3000/')
 })
